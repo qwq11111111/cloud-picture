d411c218a5690324eb9555037887cb537a260161
userHxm <1551421189@qq.com>
2025-04-22 11:08:25 +0800

    mybatisx生成user模块相关文件

---
+++ yu-picture-backend/src/main/java/com/yupi/yupicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 15514
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-22 10:58:07
+* @Entity com.yupi.yupicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ yu-picture-backend/src/main/java/com/yupi/yupicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 15514
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-04-22 10:58:07
+*/
+public interface UserService extends IService<User> {
+
+}
